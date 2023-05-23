6adbec547614250bfd83d6231bc4dffa01639d74
Christopher Weiss <mail@chris-weiss.net>
2023-05-23 20:47:13 +0200

    Show things on empty map

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"github.com/christopher-weiss/GoDoom/engine"
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"github.com/hajimehoshi/ebiten/v2/vector"
+	"image/color"
 	"log"
 )
 
@@ -13,7 +15,17 @@ const (
 	ScreenHeight int = 480
 )
 
+var things []engine.Things
+var xOff int16 = 0
+var yOff int16 = 0
+
 func main() {
+	engine.LoadWadFile("resources/doom1.wad")
+	things = engine.ReadMapData("E1M1").Things
+	for index, thing := range things {
+		fmt.Println(fmt.Sprintf("%d x: %d y: %d, dir: %d, type: %d, flags: %d", index, thing.XPosition, thing.YPosition, thing.Direction, thing.ThingType, thing.Flags))
+	}
+
 	ebiten.SetWindowSize(ScreenWidth, ScreenHeight)
 	ebiten.SetWindowTitle("Go Doom")
 
@@ -21,21 +33,33 @@ func main() {
 		log.Fatal(err)
 	}
 
-	engine.LoadWadFile("resources/doom1.wad")
-	things := engine.ReadMapData("E1M1")
-	for index, thing := range things.Things {
-		fmt.Println(fmt.Sprintf("%d x: %d y: %d, dir: %d, type: %d, flags: %d", index, thing.XPosition, thing.YPosition, thing.Direction, thing.ThingType, thing.Flags))
-	}
 }
 
 type Game struct{}
 
 func (g *Game) Update() error {
+	if ebiten.IsKeyPressed(ebiten.KeyUp) {
+		yOff = yOff + 2
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyDown) {
+		yOff = yOff - 2
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyLeft) {
+		xOff = xOff - 2
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyRight) {
+		xOff = xOff + 2
+	}
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
 	ebitenutil.DebugPrint(screen, "Loading WAD file ...")
+	for _, thing := range things {
+		x := (thing.XPosition / 10) + xOff
+		y := -((thing.YPosition / 10) + yOff)
+		vector.DrawFilledCircle(screen, float32(x), float32(y), 2.0, color.RGBA{R: 128, G: 128, B: 128, A: 128}, false)
+	}
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
